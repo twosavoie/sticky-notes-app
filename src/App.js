3b5fc4931d1b8edbf2eaa73e8034fc9d47ca6f34
lsavoie <twosavoie@gmail.com>
2021-09-19 23:47:53 -0500

    Initial commit created Sticky Notes app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Header.js";
+import NotesList from "./NotesList.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: Date.now(),
+        title: "",
+        description: "",
+        doesMatchSearch: true,
+      }
+    ],
+    searchText: ""
+  };
+  addNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true,
+    };
+    const newNotes = [newNote, ...this.state.notes];
+    this.setState({ notes: newNotes });
+  };
+  onType = (editMeId, updatedKey, updatedValue) => {
+    const updateIdMatch = (note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    };
+    const updatedNotes = this.state.notes.map(updateIdMatch);
+    this.setState({ notes: updatedNotes });
+  };
+  onSearch = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    const updatedNotes = this.state.notes.map((note) => {
+    if (!searchText) {
+      note.doesMatchSearch = true;
+      return note;
+    } else {
+      const title = note.title.toLowerCase();
+      const description = note.description.toLowerCase();
+      const titleMatch = title.includes(searchText);
+      const descriptionMatch = description.includes(searchText);
+      const hasMatch = titleMatch || descriptionMatch;
+      note.doesMatchSearch = hasMatch;
+      return note;
+    }
+    });
+    this.setState({
+      searchText: searchText, 
+      notes: updatedNotes
+    });
+  };
+  remove = (deleteMeId) => {
+    const notIdMatch = (note) => note.id !== deleteMeId;
+    const updatedNotes = this.state.notes.filter(notIdMatch);
+    this.setState({ notes: updatedNotes });
+  };
+  componentDidMount() {
+    const stringifiedNotes = localStorage.getItem("savedNotes");
+    if (stringifiedNotes) {
+      const savedNotes = JSON.parse(stringifiedNotes);
+      this.setState({ notes: savedNotes });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Header searchText={this.state.searchText} addNote={this.addNote} onSearch={this.onSearch}/>
+        <NotesList notes={this.state.notes} onType={this.onType} remove={this.remove} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Note.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Note = (props) => {
+    const updateTitle = (e) => {
+        const updatedValue = e.target.value;
+        const editMeId = props.note.id;
+        props.onType(editMeId, "title", updatedValue);
+    };
+    const updateDescription = (e) => {
+        const updatedValue = e.target.value;
+        const editMeId = props.note.id;
+        props.onType(editMeId, "description", updatedValue);
+    };
+    const clickDelete = () => props.remove(props.note.id);
+
+    return (
+        <li className="note">
+            <input type="text" placeholder="Title" className="note__title" value={props.note.title} onChange={updateTitle} />
+            <textarea placeholder="Description..." className="note__description" value={props.note.description} onChange={updateDescription} />
+            <span onClick={clickDelete} className="note__delete">X</span>
+        </li>
+    );
+};
+
+export default Note;
